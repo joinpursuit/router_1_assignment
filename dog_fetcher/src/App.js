83831604adb87c680af0890a6522b20d2b6f874e
Michell Tejada <michelltejada@pursuit.com>
2019-01-28 15:37:48 -0500

    Something

--- dog_fetcher/src/App.js
+++ dog_fetcher/src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom'
+import NavBar from './NavBar.js'
+import RandomDog from './RandomDog'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <NavBar />
+        <Switch>
+          <Route
+          exact path='/'
+          render={() =>
+            {return <h1>Home</h1>}} //{no component required, can just return jsx}
+          />
+          <Route
+          path='/random'
+          component={RandomDog}
+
+          />
+          <Route
+          path='/by_breed'
+          render={() =>
+            {return <h1>By Breed!</h1>}}
+          />
+        </Switch>
       </div>
     );
   }
--- dog_fetcher/src/Image.js
+++ dog_fetcher/src/Image.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
-
+const Image = ({ imgUrl }) => {
+  return (
+    <img src={ imgUrl} className='dog_img' alt=''/>
+  )
+}
 
 export default Image
